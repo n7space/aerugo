5df7f97a26af1799cde3121784d2584102f9b49a
Filip Demski <glamhoth@protonmail.com>
2023-04-12 16:03:59 +0200

    Build setup

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_semihosting;
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::debug;
+
+#[entry]
+fn main() -> ! {
+    asm::nop();
+
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
