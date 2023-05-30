040c7c7fe6b0bf21b60d214dcf22cebaa79a9907
Filip Demski <glamhoth@protonmail.com>
2023-05-30 15:29:08 +0200

    Build: Created examples for both targets

---
+++ examples/x86-hello-world/src/main.rs
@@ -0,0 +1,45 @@
+#![allow(non_upper_case_globals)]
+
+use aerugo::{Aerugo, InitApi, MessageQueueStorage, TaskletConfiguration, TaskletStorage};
+
+static AERUGO: Aerugo = Aerugo::new();
+
+#[allow(dead_code)]
+struct TaskAData {
+    a: u8,
+    b: u32,
+}
+
+#[allow(dead_code)]
+struct TaskBData {
+    a: u16,
+    b: u16,
+}
+
+static tasklet_a: TaskletStorage<u8, TaskAData> = TaskletStorage::new();
+static tasklet_b: TaskletStorage<u8, TaskBData> = TaskletStorage::new();
+static queue_x: MessageQueueStorage<u8, 16> = MessageQueueStorage::new();
+
+fn main() -> ! {
+    AERUGO
+        .create_tasklet(TaskletConfiguration::default(), &tasklet_a)
+        .unwrap();
+    let tasklet_a_handle = tasklet_a.create_task_handle().unwrap();
+
+    AERUGO
+        .create_tasklet(TaskletConfiguration::default(), &tasklet_b)
+        .unwrap();
+    let tasklet_b_handle = tasklet_b.create_task_handle().unwrap();
+
+    AERUGO.create_message_queue(&queue_x).unwrap();
+    let queue_x_handle = queue_x.create_queue_handle().unwrap();
+
+    AERUGO
+        .subscribe_tasklet_to_queue(&tasklet_a_handle, &queue_x_handle)
+        .unwrap();
+    AERUGO
+        .subscribe_tasklet_to_queue(&tasklet_b_handle, &queue_x_handle)
+        .unwrap();
+
+    AERUGO.start_scheduler();
+}
