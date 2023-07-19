c29091152ee3b5d43465a6a736c2d1bec1d9dd5a
Filip Demski <glamhoth@protonmail.com>
2023-07-19 10:51:31 +0200

    Tasklet: Test for const size

--- src/tasklet.rs
+++ src/tasklet.rs
@@ -133,3 +133,36 @@ impl<T, C> DataReceiver<T> for Tasklet<T, C> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn const_size() {
+        type TaskletStub = Tasklet<(), ()>;
+        let stub_size = core::mem::size_of::<TaskletStub>();
+
+        struct NoCtx;
+        type TaskletNoCtx = Tasklet<u8, NoCtx>;
+        let noctx_size = core::mem::size_of::<TaskletNoCtx>();
+
+        struct SmallCtx {
+            _a: u16,
+        }
+        type TaskletSmallCtx = Tasklet<u16, SmallCtx>;
+        let smallctx_size = core::mem::size_of::<TaskletSmallCtx>();
+
+        struct BigCtx {
+            _a: u64,
+            _b: f64,
+            _c: u16,
+        }
+        type TaskletBigCtx = Tasklet<u32, BigCtx>;
+        let bigctx_size = core::mem::size_of::<TaskletBigCtx>();
+
+        assert_eq!(noctx_size, stub_size);
+        assert_eq!(smallctx_size, stub_size);
+        assert_eq!(bigctx_size, stub_size);
+    }
+}
