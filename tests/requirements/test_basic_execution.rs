0d8926e2a953a50a1663dabe1482914898010ecf
Filip Demski <glamhoth@protonmail.com>
2023-07-26 16:18:22 +0200

    Tests: Added `ROS-FUN-RTOS-110` requirement

--- tests/requirements/test_basic_execution.rs
+++ tests/requirements/test_basic_execution.rs
@@ -2,6 +2,7 @@ use assert_cmd::Command;
 use test_binary::build_test_binary;
 
 /// @SRS{ROS-FUN-RTOS-10}
+/// @SRS{ROS-FUN-RTOS-110}
 #[cfg_attr(not(doc), test)]
 fn req_test_basic_execution() {
     let test_bin_path =
