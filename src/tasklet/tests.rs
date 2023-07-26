4c44fef3133ca94ac71e19ab28902fb532d668b9
Filip Demski <glamhoth@protonmail.com>
2023-07-26 11:42:28 +0200

    Tasklet: Added `tasklet_execution_status` test

--- src/tasklet/tests.rs
+++ src/tasklet/tests.rs
@@ -1,5 +1,58 @@
 use super::*;
 
+struct MockDataProvider {
+    data_ready: bool,
+}
+
+impl MockDataProvider {
+    const fn new() -> Self {
+        MockDataProvider { data_ready: false }
+    }
+
+    fn set_data_ready(&mut self, data_ready: bool) {
+        self.data_ready = data_ready
+    }
+}
+
+impl DataProvider<()> for MockDataProvider {
+    fn data_ready(&self) -> bool {
+        self.data_ready
+    }
+
+    fn get_data(&self) -> Option<()> {
+        if self.data_ready {
+            Some(())
+        } else {
+            None
+        }
+    }
+}
+
+/// @SRS{ROS-FUN-RTOS-50}
+/// @SRS{ROS-FUN-RTOS-70}
+/// @SRS{ROS-FUN-RTOS-80}
+#[cfg_attr(not(doc), test)]
+fn tasklet_execution_state() {
+    static mut MOCK_DATA_PROVIDER: MockDataProvider = MockDataProvider::new();
+
+    static mut TASKLET_CONTEXT: () = ();
+    let tasklet_config = TaskletConfig {
+        name: "TestTasklet",
+    };
+    let tasklet: Tasklet<(), ()> =
+        Tasklet::new(tasklet_config, |_, _| {}, unsafe { &mut TASKLET_CONTEXT });
+
+    let subscribe_result = unsafe { tasklet.subscribe(&MOCK_DATA_PROVIDER) };
+    assert!(subscribe_result.is_ok());
+
+    // Execution status is managed by the executor.
+    assert_eq!(tasklet.get_status(), TaskStatus::Sleeping);
+
+    assert!(!tasklet.is_ready());
+    unsafe { MOCK_DATA_PROVIDER.set_data_ready(true); };
+    assert!(tasklet.is_ready());
+}
+
 #[test]
 fn const_size() {
     type TaskletStub = Tasklet<(), ()>;
