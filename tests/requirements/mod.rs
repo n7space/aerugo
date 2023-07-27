e4d1f8c88a10fb3a3f944c013ce4f363cdf068e5
Filip Demski <glamhoth@protonmail.com>
2023-07-27 13:11:29 +0200

    Tests: Created `test-tasklet-priority-multiple-queues`

--- tests/requirements/mod.rs
+++ tests/requirements/mod.rs
@@ -1,3 +1,4 @@
 mod test_basic_execution;
 mod test_message_queue;
 mod test_tasklet_priority;
+mod test_tasklet_priority_multiple_queues;
---
+++ tests/requirements/test_tasklet_priority_multiple_queues.rs
@@ -0,0 +1,32 @@
+use assert_cmd::Command;
+use test_binary::build_test_binary;
+
+/// @SRS{ROS-FUN-RTOS-10}
+/// @SRS{ROS-FUN-RTOS-90}
+/// @SRS{ROS-FUN-RTOS-100}
+/// @SRS{ROS-FUN-RTOS-110}
+#[cfg_attr(not(doc), test)]
+fn req_test_tasklet_priority_multiple_queues() {
+    let test_bin_path = build_test_binary("test-tasklet-priority-multiple-queues", "testbins")
+        .expect("error building test binary");
+
+    Command::new(test_bin_path)
+        .timeout(std::time::Duration::from_secs(1))
+        .assert()
+        .success()
+        .code(0)
+        .stdout(
+            r#"TaskB: 1
+TaskB: 2
+TaskC: 1
+TaskB: 3
+TaskB: 4
+TaskC: 2
+TaskB: 5
+TaskB: 6
+TaskC: 3
+TaskC: 4
+TaskC: 5
+"#,
+        );
+}
