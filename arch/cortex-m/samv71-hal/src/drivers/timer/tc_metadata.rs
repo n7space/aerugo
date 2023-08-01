84bc086e1db3e6a348b9b98994d1d030f2543788
Wojciech Olech <wojciech.olech@n7space.com>
2023-08-01 17:27:58 +0200

    HAL: Added missing metadata for Timer instances

--- arch/cortex-m/samv71-hal/src/drivers/timer/tc_metadata.rs
+++ arch/cortex-m/samv71-hal/src/drivers/timer/tc_metadata.rs
@@ -1,6 +1,6 @@
 //! Module with PAC TC metadata implementation.
 pub(super) use pac::tc0::RegisterBlock;
-use pac::{Interrupt, TC0};
+use pac::{Interrupt, TC0, TC1, TC2, TC3};
 
 /// Amount of channels per timer instance.
 const CHANNELS_COUNT_PER_TIMER: usize = 3;
@@ -18,4 +18,22 @@ impl TcMetadata for TC0 {
     const REGISTERS: *const RegisterBlock = TC0::PTR;
     const INTERRUPTS: [Interrupt; CHANNELS_COUNT_PER_TIMER] =
         [Interrupt::TC0, Interrupt::TC1, Interrupt::TC2];
+}
+
+impl TcMetadata for TC1 {
+    const REGISTERS: *const RegisterBlock = TC1::PTR;
+    const INTERRUPTS: [Interrupt; CHANNELS_COUNT_PER_TIMER] =
+        [Interrupt::TC3, Interrupt::TC4, Interrupt::TC5];
+}
+
+impl TcMetadata for TC2 {
+    const REGISTERS: *const RegisterBlock = TC2::PTR;
+    const INTERRUPTS: [Interrupt; CHANNELS_COUNT_PER_TIMER] =
+        [Interrupt::TC6, Interrupt::TC7, Interrupt::TC8];
+}
+
+impl TcMetadata for TC3 {
+    const REGISTERS: *const RegisterBlock = TC3::PTR;
+    const INTERRUPTS: [Interrupt; CHANNELS_COUNT_PER_TIMER] =
+        [Interrupt::TC9, Interrupt::TC10, Interrupt::TC11];
 }
