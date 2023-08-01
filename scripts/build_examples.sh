52df24a9ab856089d771b012a5273a35f030a2f3
Filip Demski <glamhoth@protonmail.com>
2023-08-01 16:44:33 +0200

    Scripts: Created `build_examples` script

---
+++ scripts/build_examples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    for d in examples/*/; do
+        pushd $d > /dev/null
+        cargo build
+        popd > /dev/null
+    done
+else
+    pushd examples/$1/ > /dev/null
+    cargo build
+    popd > /dev/null
+fi
