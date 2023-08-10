732199dbb9e186dfe7cdad01d4b42456894318d5
Wojciech Olech <wojciech.olech@n7space.com>
2023-08-10 20:45:09 +0200

    Calldwell: Updated unit test app example, added separate test init

--- calldwell/examples/unit_test_app/src/main.rs
+++ calldwell/examples/unit_test_app/src/main.rs
@@ -6,23 +6,39 @@ extern crate cortex_m_rt;
 extern crate panic_rtt_target;
 extern crate rtt_target;
 
-use core::str::from_utf8;
-use cortex_m::asm::nop;
+use core::{cell::RefCell, str::from_utf8};
+use cortex_m::{asm::nop, interrupt as irq, interrupt::Mutex};
 use cortex_m_rt::entry;
 use rtt_target::{rprint, rprintln, rtt_init_default, set_print_channel, DownChannel};
 
+static STDIN: Mutex<RefCell<Option<DownChannel>>> = Mutex::new(RefCell::new(None));
+
+fn init_test() {
+    irq::free(|cs| {
+        let rtt = rtt_init_default!();
+        set_print_channel(rtt.up.0);
+        STDIN.borrow(cs).replace(Some(rtt.down.0));
+    });
+}
+
+fn read_stdin(buffer: &mut [u8]) -> usize {
+    irq::free(|cs| {
+        let mut stream_ref = STDIN.borrow(cs).borrow_mut();
+        let stream = stream_ref.as_mut().unwrap();
+        stream.read(buffer)
+    })
+}
+
 #[entry]
 fn main() -> ! {
-    let rtt = rtt_init_default!();
-    set_print_channel(rtt.up.0);
-    let mut input: DownChannel = rtt.down.0;
+    init_test();
     let mut input_received = false;
 
     rprintln!("Hello from RTT!");
 
     while !input_received {
         let mut buffer: [u8; 32] = [0; 32];
-        let received = input.read(&mut buffer);
+        let received = read_stdin(&mut buffer);
         if received > 0 {
             rprintln!(
                 "Received '{}'",
