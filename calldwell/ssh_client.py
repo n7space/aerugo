a3590dbb4d502fc2dc6dde89aca986452bc4b499
Wojciech Olech <wojciech.olech@n7space.com>
2023-08-10 16:41:13 +0200

    Calldwell: Updated SSH module docs

--- calldwell/ssh_client.py
+++ calldwell/ssh_client.py
@@ -10,13 +10,13 @@ class SSHClient:
 
     def __init__(self, host: str, login: str, password: str, port: int = 22):
         """Connects with SSH server. After connecting, your working dir will be set to home dir of
-        logged in user.
+        logged in user (if it has one).
 
         # Parameters:
-        * `host` [str] - server hostname (can be an IP address)
-        * `login` [str] - user login
-        * `password` [str] - user password
-        * `port` [int] - SSH port, default: 22
+        * `host` - server hostname (can be an IP address)
+        * `login` - user login
+        * `password` - user password
+        * `port` - SSH port, default: 22
         """
         self.client = paramiko.SSHClient()
         self.client.load_system_host_keys()
@@ -35,14 +35,13 @@ class SSHClient:
         timeout: Optional[float] = None,
         environment: Optional[Dict[str, str]] = None,
     ) -> CommandChannels:
-        """Executes a command on remote, returns `stdin`, `stdout`, `stderr` triple
+        """Executes a command on remote, returns `stdin`, `stdout`, `stderr` wrapped in dataclass.
 
         # Parameters
-        * `command` [str] - command (and arguments) to be executed, in form of a single string
-        * `timeout` [Optional[float]] - time, in seconds, to wait for pending I/O operation before
-                                        an exception is raised, or `None` to disable timeout.
-        * `environment` [Optional[Dict[str, str]]] - additional environment variables for executed
-                                                  program.
+        * `command` - command (and arguments) to be executed, in form of a single string
+        * `timeout` - time, in seconds, to wait for pending I/O operation before an exception is
+                      raised, or `None` to disable timeout.
+        * `environment` - additional environment variables for executed program.
         """
 
         stdin, stdout, stderr = self.client.exec_command(
@@ -54,8 +53,8 @@ class SSHClient:
         """Sends a file from local machine to remote. Will raise an exception on failure.
 
         # Parameters
-        * `local_source_path` [str] - path to local file that will be uploaded
-        * `remote_destination_path` [str] - path on remote machine where the file will be uploaded
+        * `local_source_path` - path to local file that will be uploaded
+        * `remote_destination_path` - path on remote machine where the file will be uploaded
         """
         self.sftp.put(local_source_path, remote_destination_path, confirm=True)
 
@@ -63,8 +62,8 @@ class SSHClient:
         """Downloads a file from remote to local machine. Will raise an exception on failure.
 
         # Parameters
-        * `remote_source_path` [str] - remote path to downloaded file
-        * `local_destination_path` [str] - local destination path for downloaded file
+        * `remote_source_path` - remote path to downloaded file
+        * `local_destination_path` - local destination path for downloaded file
         """
         self.sftp.get(remote_source_path, local_destination_path)
 
