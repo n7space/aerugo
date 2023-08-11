ac4badbeab191d02850f06f1970e2ee4e0b187ec
Filip Demski <glamhoth@protonmail.com>
2023-08-11 14:59:39 +0200

    Tests: Added `test-event-cancellation` integration tests

--- tests/requirements/mod.rs
+++ tests/requirements/mod.rs
@@ -1,5 +1,6 @@
 mod test_basic_execution;
 mod test_boolean_condition;
+mod test_event_cancellation;
 mod test_events;
 mod test_message_queue;
 mod test_tasklet_priority;
---
+++ tests/requirements/test_event_cancellation.rs
@@ -0,0 +1,27 @@
+use assert_cmd::Command;
+use test_binary::build_test_binary;
+
+/// @SRS{ROS-FUN-RTOS-10}
+/// @SRS{ROS-FUN-RTOS-3010}
+/// @SRS{ROS-FUN-RTOS-3020}
+/// @SRS{ROS-FUN-RTOS-3050}
+/// @SRS{ROS-FUN-RTOS-3070}
+/// @SRS[ROS-FUN-RTOS-3080]
+/// @SRS{ROS-FUN-RTOS-3090}
+#[cfg_attr(not(doc), test)]
+fn req_test_events() {
+    let test_bin_path = build_test_binary("test-event-cancellation", "testbins")
+        .expect("error building test binary");
+
+    Command::new(test_bin_path)
+        .timeout(std::time::Duration::from_secs(1))
+        .assert()
+        .success()
+        .code(0)
+        .stdout(
+            r#"TaskB: 1
+TaskC: 42
+TaskC: 255
+"#,
+        );
+}
