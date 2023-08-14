c9fb3883d76e899bf5ee12af3d4cf84c7c14eb6f
Filip Demski <glamhoth@protonmail.com>
2023-08-14 15:15:59 +0200

    Tests: Covered design requirements

---
+++ tests/requirements/design.rs
@@ -0,0 +1,107 @@
+/// @SRS{ROS-FUN-RTOS-20}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_tasklet_persistent_context_data() {}
+
+/// @SRS{ROS-FUN-RTOS-30}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_tasklet_user_data_safe_access() {}
+
+/// @SRS{ROS-FUN-RTOS-40}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_safe_access_to_shared_data() {}
+
+/// @SRS{ROS-FUN-BSP-WDT-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_wdt() {}
+
+/// @SRS{ROS-FUN-BSP-NVIC-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_nvic() {}
+
+/// @SRS{ROS-FUN-BSP-SCB-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_scb() {}
+
+/// @SRS{ROS-FUN-BSP-SYST-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_syst() {}
+
+/// @SRS{ROS-FUN-BSP-PMC-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_pcm() {}
+
+/// @SRS{ROS-FUN-BSP-UART-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_uart() {}
+
+/// @SRS{ROS-FUN-BSP-PIO-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_pio() {}
+
+/// @SRS{ROS-FUN-BSP-SPI-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_spi() {}
+
+/// @SRS{ROS-FUN-BSP-TIC-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_tic() {}
+
+/// @SRS{ROS-FUN-BSP-XDMAC-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_xdmac() {}
+
+/// @SRS{ROS-FUN-BSP-FPU-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_provide_driver_fpu() {}
+
+/// @SRS{ROS-RES-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_bsp_target_samv71() {}
+
+/// @SRS{ROS-DES-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_bsp_implemented_in_rust() {}
+
+/// @SRS{ROS-DES-20}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_implemented_in_rust() {}
+
+/// @SRS{ROS-POR-10}
+///
+/// Design Analisys is described in N7S-ROS-SVSR-001, chapter 6.2.
+#[cfg_attr(not(doc), test)]
+fn des_ros_partitioned_into_platform_specific_and_agnostic_code() {}
--- tests/requirements/mod.rs
+++ tests/requirements/mod.rs
@@ -1,1 +1,2 @@
+mod design;
 mod test;
