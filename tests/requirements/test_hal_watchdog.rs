b4c4e78e042715140f51851364d251c1464b3bac
Wojciech Olech <wojciech.olech@n7space.com>
2023-08-16 12:54:45 +0200

    Tests: Temporarily disabled V71 integration tests on CI, due to lack of support for them in our current Github Actions config

--- tests/requirements/test_hal_watchdog.rs
+++ tests/requirements/test_hal_watchdog.rs
@@ -4,12 +4,13 @@ use assert_cmd::Command;
 /// @SRS{ROS-FUN-BSP-WDT-20}
 /// @SRS{ROS-FUN-BSP-WDT-30}
 #[cfg_attr(not(doc), test)]
-fn req_test_watchdog() {
+#[ignore]
+fn req_test_hal_watchdog() {
     // TODO: Fix building the binary
     // build_test_binary("test-hal-watchdog", "testbins").expect("error building test binary");
 
     Command::new("python")
-        .arg("tests/requirements/test_watchdog.py")
+        .arg("tests/requirements/test_hal_watchdog.py")
         .timeout(std::time::Duration::from_secs(30))
         .assert()
         .success()
