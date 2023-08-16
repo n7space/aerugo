c555279f9c5b8f8325bb2488649eb3750f383ab3
Filip Demski <glamhoth@protonmail.com>
2023-08-16 17:42:44 +0200

    Tests: Modified requirements test structure

---
+++ tests/requirements/test/mod.rs
@@ -0,0 +1,10 @@
+mod test_basic_execution;
+mod test_boolean_condition;
+mod test_event_cancellation;
+mod test_event_clear_queue;
+mod test_events;
+mod test_hal_timer;
+mod test_hal_watchdog;
+mod test_message_queue;
+mod test_tasklet_priority;
+mod test_tasklet_priority_multiple_queues;
