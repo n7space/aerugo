0c4741182e199e21736186fed98dd4d5741372c6
Wojciech Olech <wojciech.olech@n7space.com>
2023-08-18 11:10:12 +0200

    Calldwell-rs: Refactored unit test example to use calldwell-rs init function

--- calldwell/examples/unit_test_app/src/main.rs
+++ calldwell/examples/unit_test_app/src/main.rs
@@ -7,41 +7,14 @@ extern crate cortex_m_rt;
 extern crate panic_rtt_target;
 extern crate rtt_target;
 
-use core::str::from_utf8;
-
-use calldwell::{with_rtt_in, with_rtt_out};
+use calldwell::with_rtt_out;
 use core::fmt::Write;
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    let mut input_buffer: [u8; 128] = [0; 128];
-    calldwell::initialize();
-
-    with_rtt_out(|o, _| o.write_str("Hello from SAMV71!"));
-
-    let read_bytes = with_rtt_in(|i, _| i.read(&mut input_buffer));
-
-    match read_bytes {
-        Ok(amount) => with_rtt_out(|o, _| {
-            write!(
-                o.writer(),
-                "Received {} bytes: {}",
-                amount,
-                from_utf8(&input_buffer[..amount]).expect("Invalid string received!")
-            )
-            .unwrap();
-        }),
-        Err(e) => with_rtt_out(|o, _| {
-            write!(
-                o.writer(),
-                "Error occurred while receiving message: {:?}",
-                e
-            )
-            .unwrap();
-        }),
-    }
+    calldwell::start_test_session();
 
     with_rtt_out(|o, _| o.write_str("Running tests..."));
 
