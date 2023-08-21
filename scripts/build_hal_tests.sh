d3921806e2f3c33def8bad015ece9598fe2096e8
Wojciech Olech <wojciech_olech@hotmail.com>
2023-08-21 08:55:01 +0200

    Scripts: Fixed build script for HAL tests and Rust CI workflow
    Co-authored-by: Filip Demski (Glamhoth) <Glamhoth@users.noreply.github.com>

--- scripts/build_hal_tests.sh
+++ scripts/build_hal_tests.sh
@@ -2,15 +2,8 @@
 
 set -euo pipefail
 
-if [ $# -eq 0 ]
-then
-    for d in testbins/test-hal-*/; do
-        pushd $d > /dev/null
-        cargo build
-        popd > /dev/null
-    done
-else
-    pushd examples/$1/ > /dev/null
-    cargo build
-    popd > /dev/null
-fi
+for d in testbins/test-hal-*/; do
+  pushd $d > /dev/null
+  cargo build
+  popd > /dev/null
+done
