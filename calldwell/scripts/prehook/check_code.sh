e57e03fd1f76ee17bda33c8312bf38f7ada667bb
Filip Demski <glamhoth@protonmail.com>
2023-08-29 11:24:05 +0200

    Scripts: Added check whether any of the python code changed

--- calldwell/scripts/prehook/check_code.sh
+++ calldwell/scripts/prehook/check_code.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-poetry run isort --check ./*.py
-poetry run black --check ./*.py
-poetry run flake8 ./*.py
-poetry run mypy ./*.py
+if [[ "$(git diff --diff-filter=d --dirstat=files,0,cumulative --cached -- './*.py')" ]]; then
+    poetry run isort --check ./*.py
+    poetry run black --check ./*.py
+    poetry run flake8 ./*.py
+    poetry run mypy ./*.py
+fi
 
 # Check examples if changed
 if [[ "$(git diff --diff-filter=d --dirstat=files,0,cumulative --cached)" =~ "examples/" ]]; then
