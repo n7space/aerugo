70de36e8d1db0dee9117c707516e02e07b427930
Filip Demski <glamhoth@protonmail.com>
2023-09-01 14:10:08 +0200

    Event: Replaced `InternalCell` with `InternalList`

--- src/event.rs
+++ src/event.rs
@@ -7,35 +7,33 @@ pub use self::event_enabler::EventEnabler;
 
 pub(crate) use self::event_set::EventSet;
 
-use heapless::Vec;
-use internal_cell::InternalCell;
-
 use crate::aerugo::Aerugo;
 use crate::api::{InitError, RuntimeError};
+use crate::internal_list::InternalList;
 
 /// Type for list of event sets.
-type EventSetList = Vec<&'static EventSet, { Aerugo::TASKLET_COUNT }>;
+type EventSetList = InternalList<&'static EventSet, { Aerugo::TASKLET_COUNT }>;
 
 /// System event used for sending signals between tasklets.
 pub(crate) struct Event {
     /// ID of this event.
-    event_id: EventId,
-    /// List of sets that this tasklet is in.
-    sets: InternalCell<EventSetList>,
+    id: EventId,
+    /// List of sets that this event is in.
+    sets: EventSetList,
 }
 
 impl Event {
     /// Creates new event.
-    pub(crate) fn new(event_id: EventId) -> Self {
+    pub(crate) fn new(id: EventId) -> Self {
         Self {
-            event_id,
-            sets: EventSetList::new().into(),
+            id,
+            sets: EventSetList::new(),
         }
     }
 
     /// Returns ID of this event.
     pub(crate) fn id(&self) -> EventId {
-        self.event_id
+        self.id
     }
 
     /// Adds new set to the list.
@@ -50,7 +48,7 @@ impl Event {
     /// This is unsafe, because it mutably borrows the list of event sets.
     /// This is safe to call before the system initialization.
     pub(crate) unsafe fn add_set(&self, event_set: &'static EventSet) -> Result<(), InitError> {
-        match self.sets.as_mut_ref().push(event_set) {
+        match self.sets.add(event_set) {
             Ok(_) => Ok(()),
             Err(_) => Err(InitError::EventSetListFull),
         }
@@ -64,8 +62,8 @@ impl Event {
     /// # Return
     /// `()` if successful, `RuntimeError` otherwise
     pub(crate) fn emit(&self) -> Result<(), RuntimeError> {
-        for event_set in unsafe { self.sets.as_ref() } {
-            event_set.activate_event(self.event_id)?;
+        for event_set in &self.sets {
+            event_set.activate_event(self.id)?;
         }
 
         Ok(())
@@ -78,8 +76,8 @@ impl Event {
     /// # Return
     /// `()` if successful, `RuntimeError` otherwise
     pub(crate) fn cancel(&self) -> Result<(), RuntimeError> {
-        for event_set in unsafe { self.sets.as_ref() } {
-            event_set.deactivate_event(self.event_id)?;
+        for event_set in &self.sets {
+            event_set.deactivate_event(self.id)?;
         }
 
         Ok(())
