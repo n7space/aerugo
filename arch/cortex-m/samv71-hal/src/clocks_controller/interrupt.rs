c5d750fa323c4e08316ce20c86e5e7abd848b754
Wojciech Olech <wojciech.olech@n7space.com>
2023-09-08 16:25:28 +0200

    ClocksController: Added convenience functions for `Interrupts` structure

--- arch/cortex-m/samv71-hal/src/clocks_controller/interrupt.rs
+++ arch/cortex-m/samv71-hal/src/clocks_controller/interrupt.rs
@@ -25,3 +25,36 @@ pub struct Interrupts {
     /// Slow crystal oscillator error interrupt.
     pub slow_oscillator_error: bool,
 }
+
+impl Interrupts {
+    /// Creates [`Interrupts`] instance with all interrupts flags set.
+    pub const fn all() -> Self {
+        Interrupts {
+            mco_stabilized: true,
+            plla_locked: true,
+            master_clock_ready: true,
+            utmi_pll_locked: true,
+            pck_ready: [true; 8],
+            main_clock_selected: true,
+            main_rc_stabilized: true,
+            mco_failure_event_detected: true,
+            slow_oscillator_error: true,
+        }
+    }
+
+    /// Creates [`Interrupts`] instance with all interrupts flags cleared.
+    /// Same as [`Default::default`], but with `const` on top.
+    pub const fn none() -> Self {
+        Interrupts {
+            mco_stabilized: false,
+            plla_locked: false,
+            master_clock_ready: false,
+            utmi_pll_locked: false,
+            pck_ready: [false; 8],
+            main_clock_selected: false,
+            main_rc_stabilized: false,
+            mco_failure_event_detected: false,
+            slow_oscillator_error: false,
+        }
+    }
+}
