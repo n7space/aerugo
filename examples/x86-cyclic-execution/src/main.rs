0326a80b58ff8f77a41efcdaf0b104b0b56e33b0
Filip Demski <glamhoth@protonmail.com>
2023-09-19 15:57:46 +0200

    Examples: Added x86-cyclic-execution example

---
+++ examples/x86-cyclic-execution/src/main.rs
@@ -0,0 +1,56 @@
+use aerugo::{
+    logln, Aerugo, Duration, InitApi, RuntimeApi, SystemHardwareConfig, TaskletConfig,
+    TaskletStorage,
+};
+
+#[derive(Default)]
+struct TaskAContext {
+    cnt: u8,
+}
+
+fn task_a(_: (), context: &mut TaskAContext, _: &dyn RuntimeApi) {
+    context.cnt = context.cnt.wrapping_add(1);
+
+    logln!("TaskA: {}", context.cnt);
+}
+
+#[derive(Default)]
+struct TaskBContext {
+    cnt: u8,
+}
+
+fn task_b(_: (), context: &mut TaskBContext, _: &dyn RuntimeApi) {
+    context.cnt = context.cnt.wrapping_add(1);
+
+    logln!("TaskB: {}", context.cnt);
+}
+
+static TASK_A_STORAGE: TaskletStorage<(), TaskAContext, 0> = TaskletStorage::new();
+static TASK_B_STORAGE: TaskletStorage<(), TaskBContext, 0> = TaskletStorage::new();
+
+fn main() -> ! {
+    let (aerugo, _) = Aerugo::initialize(SystemHardwareConfig::default());
+
+    let task_a_config = TaskletConfig {
+        name: "TaskA",
+        ..Default::default()
+    };
+    let task_a_context = TaskAContext { cnt: 0 };
+
+    let task_b_config = TaskletConfig {
+        name: "TaskB",
+        ..Default::default()
+    };
+    let task_b_context = TaskBContext { cnt: 0 };
+
+    aerugo.create_tasklet_with_context(task_a_config, task_a, task_a_context, &TASK_A_STORAGE);
+    aerugo.create_tasklet_with_context(task_b_config, task_b, task_b_context, &TASK_B_STORAGE);
+
+    let task_a_handle = TASK_A_STORAGE.create_handle().unwrap();
+    aerugo.subscribe_tasklet_to_cyclic(&task_a_handle, Some(Duration::secs(1)));
+
+    let task_b_handle = TASK_B_STORAGE.create_handle().unwrap();
+    aerugo.subscribe_tasklet_to_cyclic(&task_b_handle, Some(Duration::secs(5)));
+
+    aerugo.start();
+}
