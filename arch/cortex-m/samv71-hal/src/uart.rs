8dac5a56a451ebb783d31b97cad713b4fa9cc445
Wojciech Olech <wojciech.olech@n7space.com>
2023-09-30 12:49:38 +0200

    UART: Added PAC metadata trait for type erasure, and module description

--- arch/cortex-m/samv71-hal/src/uart.rs
+++ arch/cortex-m/samv71-hal/src/uart.rs
@@ -1,1 +1,25 @@
 //! Module containing Universal Asynchronous Receiver/Transmitter (UART) HAL driver.
+//!
+//! Before using UART, make sure to
+//! - Enable UART peripheral clock using PMC driver
+//! - Set appropriate pins mode to peripheral mode using PIO driver
+//! Consult the SAMV71 datasheet for more information.
+//!
+//! This driver allows you to configure and use any available UART driver in safe way.
+//! Currently, the driver supports:
+//! - Baudrate and source clock configuration
+//! - Transmitter and receiver status checks and configuration
+//! - Mode configuration (normal/echo/loopback)
+//! - Parity configuration
+//! - Parity/overrun/framing error detection
+//! - Interrupt configuration
+//! - Digital filter configuration
+//!
+//! Currently, it does NOT support:
+//! - Comparison configuration
+//! - Sleepwalking mode
+
+pub mod metadata;
+
+/// Structure representing UART driver
+pub struct UART {}
