2ac0dc4ce9a17fd1683d7684f0e281f4036dfc63
Wojciech Olech <wojciech.olech@n7space.com>
2023-10-01 18:00:29 +0200

    UART: Added control register functions

--- arch/cortex-m/samv71-hal/src/uart.rs
+++ arch/cortex-m/samv71-hal/src/uart.rs
@@ -31,7 +31,9 @@
 
 use core::marker::PhantomData;
 
-use self::metadata::UartMetadata;
+use self::metadata::{RegisterBlock, UartMetadata};
+
+pub use self::status::Status;
 
 pub mod interrupt;
 pub mod metadata;
@@ -44,4 +46,72 @@ pub struct UART<Metadata: UartMetadata> {
     _meta: PhantomData<Metadata>,
 }
 
-impl<Metadata: UartMetadata> UART<Metadata> {}
+impl<Instance: UartMetadata> UART<Instance> {
+    /// Returns current UART status.
+    ///
+    /// Status flags must be manually cleared by calling [`UART::reset_status`](Self::reset_status)
+    pub fn status(&self) -> Status {
+        todo!()
+    }
+
+    /// Resets UART status by clearing status flags.
+    /// **This function should usually be called immediately after reading the status.**
+    pub fn reset_status(&mut self) {
+        self.registers_ref().cr.write(|w| w.rststa().set_bit());
+    }
+
+    /// Enables UART transmitter.
+    ///
+    /// By default, the transmitter is disabled and it must be enabled before trying to
+    /// transmit the data.
+    pub fn enable_transmitter(&mut self) {
+        self.registers_ref().cr.write(|w| w.txen().set_bit());
+    }
+
+    /// Disables UART transmitter.
+    ///
+    /// If a character is being processed, and a character has been written to UART holding
+    /// register, both characters are transmitted before the transmitter is stopped.
+    pub fn disable_transmitter(&mut self) {
+        self.registers_ref().cr.write(|w| w.txdis().set_bit());
+    }
+
+    /// Resets UART transmitter.
+    ///
+    /// Any pending character transmission is aborted when the transmitter is reset.
+    pub fn reset_transmitter(&mut self) {
+        self.registers_ref().cr.write(|w| w.rsttx().set_bit());
+    }
+
+    /// Enables UART receiver.
+    ///
+    /// By default, the receiver is disabled and it must be enabled before trying to
+    /// receive the data.
+    pub fn enable_receiver(&mut self) {
+        self.registers_ref().cr.write(|w| w.rxen().set_bit());
+    }
+
+    /// Disables UART receiver.
+    ///
+    /// If a character is being processed, reception is completed before receiver is stopped.
+    pub fn disable_receiver(&mut self) {
+        self.registers_ref().cr.write(|w| w.rxdis().set_bit());
+    }
+
+    /// Resets UART receiver.
+    ///
+    /// Any pending character reception is aborted when the receiver is reset.
+    pub fn reset_receiver(&mut self) {
+        self.registers_ref().cr.write(|w| w.rstrx().set_bit());
+    }
+
+    /// Returns reference to UART registers.
+    ///
+    /// # Safety
+    /// This function dereferences a raw pointer.
+    /// It's safe to use, as long as there aren't multiple instances
+    /// of UART sharing the same register.
+    fn registers_ref(&self) -> &RegisterBlock {
+        unsafe { &*Instance::REGISTERS }
+    }
+}
