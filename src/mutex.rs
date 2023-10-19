b7b4daa2c50fca401966865442d9cbad3d471f53
Filip Demski <glamhoth@protonmail.com>
2023-10-19 10:52:30 +0200

    Mutex: Safety documentation

--- src/mutex.rs
+++ src/mutex.rs
@@ -13,9 +13,8 @@ use core::cell::UnsafeCell;
 #[repr(transparent)]
 pub struct Mutex<T: ?Sized>(UnsafeCell<T>);
 
-/// Mutex is safe to share between threads because critical section prevents any access to the
-/// data from other threads or interrupts. Value cannot be borrowed outside of the
-/// critical section.
+/// Mutex is `Sync` because `aerugo` is a single-threaded system and critical section prevents any access
+/// to the data from interrupts. Value cannot be borrowed outside of the critical section.
 unsafe impl<T: Send + ?Sized> Sync for Mutex<T> {}
 
 impl<T> Mutex<T> {
