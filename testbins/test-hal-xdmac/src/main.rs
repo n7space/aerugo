93549d8d236caef755c8eec5086f3dda39c5fdc6
Wojciech Olech <wojciech.olech@n7space.com>
2023-10-31 15:39:38 +0100

    Tests: Added XDMAC and channel management tests

--- testbins/test-hal-xdmac/src/main.rs
+++ testbins/test-hal-xdmac/src/main.rs
@@ -6,6 +6,7 @@ extern crate calldwell;
 extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 
+use aerugo::hal::drivers::xdmac::Xdmac;
 use aerugo::time::MillisDurationU32 as WatchdogDuration;
 use aerugo::{
     hal::{drivers::nvic::NVIC, user_peripherals::PMC},
@@ -13,6 +14,8 @@ use aerugo::{
 };
 use calldwell::write_str;
 use rt::entry;
+
+mod xdmac_management;
 
 fn configure_pmc(_pmc: &mut PMC) {}
 
@@ -23,11 +26,15 @@ fn main() -> ! {
         watchdog_timeout: WatchdogDuration::secs(16),
     });
 
-    // Initialize peripheral clocks.
     let mut pmc = peripherals.pmc.take().unwrap();
+    let _nvic = NVIC::new(peripherals.nvic.take().unwrap());
+    let mut xdmac = Xdmac::new(peripherals.xdmac.take().unwrap());
+
     configure_pmc(&mut pmc);
 
-    let _nvic = NVIC::new(peripherals.nvic.take().unwrap());
+    write_str("Performing XDMAC tests...");
+
+    xdmac_management::perform_test(&mut xdmac);
 
     write_str("All XDMAC tests finished successfully.");
 
---
+++ testbins/test-hal-xdmac/src/xdmac_management.rs
@@ -0,0 +1,112 @@
+use aerugo::hal::drivers::xdmac::Xdmac;
+use calldwell::write_str;
+use heapless::Vec;
+
+const EXPECTED_NUMBER_OF_PERIPHERAL_REQUESTS: usize = 52;
+const EXPECTED_FIFO_SIZE: usize = 128;
+const EXPECTED_AVAILABLE_CHANNELS: usize = 24;
+
+pub fn perform_test(xdmac: &mut Xdmac) {
+    write_str("Performing XDMAC management tests...");
+    check_xdmac_info(xdmac);
+    check_status_reader(xdmac);
+    check_channel_taking(xdmac);
+    write_str("XDMAC management test finished successfully.");
+}
+
+pub fn check_xdmac_info(xdmac: &Xdmac) {
+    assert_eq!(
+        xdmac.number_of_peripheral_requests(),
+        EXPECTED_NUMBER_OF_PERIPHERAL_REQUESTS,
+        "unexpected amount of peripheral requests available"
+    );
+    assert_eq!(
+        xdmac.fifo_size(),
+        EXPECTED_FIFO_SIZE,
+        "unexpected FIFO size detected"
+    );
+    assert_eq!(
+        xdmac.available_channels(),
+        EXPECTED_AVAILABLE_CHANNELS,
+        "unexpected amount of available channels"
+    );
+
+    write_str("XDMAC info test finished successfully.");
+}
+
+pub fn check_status_reader(xdmac: &mut Xdmac) {
+    assert!(
+        xdmac.is_status_reader_available(),
+        "status reader has been taken out of Xdmac and not given back"
+    );
+
+    let mut status_reader = xdmac.take_status_reader().unwrap();
+
+    // There should be no pending channels (as all IRQs should be disabled)
+    assert_eq!(
+        status_reader.get_pending_channels(),
+        Vec::<_, { Xdmac::SUPPORTED_CHANNELS }>::from_slice(&[false; Xdmac::SUPPORTED_CHANNELS])
+            .unwrap(),
+        "unexpectedly, there are channels with pending interrupts"
+    );
+
+    assert!(
+        !xdmac.is_status_reader_available(),
+        "status reader was taken, but it seems available"
+    );
+    xdmac.return_status_reader(status_reader);
+
+    assert!(
+        xdmac.is_status_reader_available(),
+        "status reader has been taken out of Xdmac and not given back"
+    );
+
+    write_str("XDMAC status reader test finished successfully.");
+}
+
+pub fn check_channel_taking(xdmac: &mut Xdmac) {
+    // At this point, all the channels should be available.
+    for id in 0..Xdmac::SUPPORTED_CHANNELS {
+        assert!(
+            xdmac.is_channel_available(id),
+            "channel with ID {} is unavailable",
+            id
+        );
+    }
+
+    // Take first available channel - should have ID 0
+    let channel = xdmac.take_next_free_channel().unwrap();
+    assert_eq!(channel.id(), 0);
+
+    // Check if next taken channel will have ID 1
+    let next_channel = xdmac.take_next_free_channel().unwrap();
+    assert_eq!(next_channel.id(), 1);
+
+    // Check if both those channels are taken
+    assert!(!xdmac.is_channel_available(0));
+    assert!(!xdmac.is_channel_available(1));
+
+    // Check if it's not possible to take already taken channel
+    let taken_channel = xdmac.take_channel(0);
+    assert!(taken_channel.is_none());
+
+    // Check if it's possible to manually take a free channel
+    let taken_channel = xdmac.take_channel(10).unwrap();
+    assert_eq!(taken_channel.id(), 10);
+
+    // Put all channels back
+    xdmac.return_channel(channel);
+    xdmac.return_channel(next_channel);
+    xdmac.return_channel(taken_channel);
+
+    // Verify that they have been returned
+    for id in 0..Xdmac::SUPPORTED_CHANNELS {
+        assert!(
+            xdmac.is_channel_available(id),
+            "channel with ID {} is unavailable",
+            id
+        );
+    }
+
+    write_str("XDMAC channel take/give test finished successfully.");
+}
