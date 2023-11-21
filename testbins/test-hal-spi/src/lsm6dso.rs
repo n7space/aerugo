3a3d966c626c1a237a634677d7270e0f34b620ba
Wojciech Olech <wojciech.olech@n7space.com>
2023-11-21 13:03:32 +0100

    Tests: Implemented LSM6DSO ID check with SPI w/ XDMAC

--- testbins/test-hal-spi/src/lsm6dso.rs
+++ testbins/test-hal-spi/src/lsm6dso.rs
@@ -1,3 +1,6 @@
+use core::hint::spin_loop;
+use core::sync::atomic::{AtomicBool, Ordering};
+
 use aerugo::hal::{
     drivers::{
         nvic::{Interrupt, NVIC},
@@ -12,6 +15,7 @@ use aerugo::hal::{
         },
         xdmac::{
             channel::ChannelStatusReader,
+            events::ChannelEvents,
             status::StatusReader as XdmacStatusReader,
             transfer::{
                 AddressingMode, DataWidth, MicroblockLength, SystemBus, TransferBlock,
@@ -30,17 +34,21 @@ const LSM6DSO_CHIP: SelectedChip = SelectedChip::Chip1;
 /// LSM6DSO SPI clock divider. Default peripheral clock is 12MHz, LSM6DSO can work with up to 10MHz,
 /// but for safety a 480kHz clock will be used instead (12MHz/25).
 const LSM6DSO_SPI_CLOCK_DIVIDER: u8 = 25;
+/// LSM6DSO buffers size. Defines maximum possible transfer length.
+const LSM6DSO_BUFFER_SIZE: usize = 32;
 /// LSM6DSO SPI interface.
 #[allow(non_camel_case_types)]
 type LSM6DSO_SPI = SPI0;
 
 static mut XDMAC_STATUS_READER: Option<XdmacStatusReader> = None;
 static mut XDMAC_CHANNEL_STATUS_READER: Option<ChannelStatusReader> = None;
+static SPI_TRANSACTION_FINISHED: AtomicBool = AtomicBool::new(false);
 
 pub fn perform_test(spi: Spi<LSM6DSO_SPI, NotConfigured>, xdmac: &mut Xdmac, nvic: &mut NVIC) {
     write_str("Beginning LSM6DSO communication tests w/ XDMAC...");
 
     let mut spi = spi.into_master(MasterConfig::new(LSM6DSO_CHIP));
+    // SPI config validated with LSM6DSO datasheet.
     spi.configure_chip(
         LSM6DSO_CHIP,
         ChipConfig {
@@ -54,17 +62,76 @@ pub fn perform_test(spi: Spi<LSM6DSO_SPI, NotConfigured>, xdmac: &mut Xdmac, nvi
         },
     );
 
-    unsafe { XDMAC_STATUS_READER = xdmac.take_status_reader() };
-    let _rx_channel = xdmac.take_next_free_channel().unwrap();
-    let _tx_channel = xdmac.take_next_free_channel().unwrap();
+    // Technically, one buffer could be used for both RX and TX, but considering Rust safety rules,
+    // it might be cleaner to separate those :)
+    let mut command_buffer: [u8; LSM6DSO_BUFFER_SIZE] = [0; LSM6DSO_BUFFER_SIZE];
+    let mut response_buffer: [u8; LSM6DSO_BUFFER_SIZE] = [0; LSM6DSO_BUFFER_SIZE];
 
+    unsafe { XDMAC_STATUS_READER = xdmac.take_status_reader() };
+
+    let mut rx_channel = xdmac.take_next_free_channel().unwrap();
+    unsafe { XDMAC_CHANNEL_STATUS_READER = rx_channel.take_status_reader() };
+    rx_channel.set_events_state(ChannelEvents {
+        end_of_block: false,
+        end_of_list: true,
+        end_of_disable: false,
+        end_of_flush: false,
+        read_bus_error: true,
+        write_bus_error: true,
+        request_overflow_error: true,
+    });
+    rx_channel.enable_interrupt();
+
+    let tx_channel = xdmac.take_next_free_channel().unwrap();
+
+    // First, an ID will be read until LSM6DSO responds with valid one, to verify the connection.
+    // LSM6DSO may not respond correctly first time, but should respond eventually.
+    const READ_REQUEST: u8 = 0x80;
+    const WHO_AM_I_ADDRESS: u8 = 0x0F;
+    const WHO_AM_I_VALUE: u8 = 0x6C;
+
+    command_buffer[0] = READ_REQUEST | WHO_AM_I_ADDRESS;
+
+    // XDMAC transaction setup:
+    let mut rx_channel = rx_channel.configure_transfer(get_rx_transfer_block(
+        &spi,
+        response_buffer.as_mut_ptr() as *mut (),
+        2,
+    ));
+    let mut tx_channel = tx_channel.configure_transfer(get_tx_transfer_block(
+        &spi,
+        command_buffer.as_ptr() as *const (),
+        2,
+    ));
+
+    // Good to go, transaction can be started now.
     nvic.enable(Interrupt::XDMAC);
 
-    // TODO: Test code here
+    rx_channel.enable();
+    tx_channel.enable();
+
+    wait_for_spi_transaction();
+
+    // First transaction may not pass, so let's try again - second one should be correct.
+    rx_channel.repeat_transfer();
+    tx_channel.repeat_transfer();
+    rx_channel.enable();
+    tx_channel.enable();
+
+    wait_for_spi_transaction();
 
     nvic.disable(Interrupt::XDMAC);
 
+    assert_eq!(response_buffer[1], WHO_AM_I_VALUE);
+
     write_str("All LSM6DSO communication tests w/ XDMAC done!");
+}
+
+fn wait_for_spi_transaction() {
+    while !SPI_TRANSACTION_FINISHED.load(Ordering::SeqCst) {
+        spin_loop();
+    }
+    SPI_TRANSACTION_FINISHED.store(false, Ordering::SeqCst);
 }
 
 #[interrupt]
@@ -87,11 +154,13 @@ fn XDMAC() {
             panic!("XDMAC request overflow error");
         }
 
-        if status.end_of_block {}
+        if status.end_of_list {
+            SPI_TRANSACTION_FINISHED.store(true, Ordering::SeqCst);
+        }
     }
 }
 
-fn _get_rx_transfer_block(
+fn get_rx_transfer_block(
     spi: &Spi<LSM6DSO_SPI, Master>,
     destination: *mut (),
     data_length: u32,
@@ -118,7 +187,7 @@ fn _get_rx_transfer_block(
     .with_microblock_length(MicroblockLength::new(data_length).unwrap())
 }
 
-fn _get_tx_transfer_block(
+fn get_tx_transfer_block(
     spi: &Spi<LSM6DSO_SPI, Master>,
     source: *const (),
     data_length: u32,
