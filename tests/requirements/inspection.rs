0c6cfedb5b5aaa7e2cf69e4ecd58857724eff083
Filip Demski <glamhoth@protonmail.com>
2023-11-22 10:27:28 +0100

    Tests: Added inspections for OPER requirements

--- tests/requirements/inspection.rs
+++ tests/requirements/inspection.rs
@@ -4,6 +4,30 @@
 #[cfg_attr(not(doc), test)]
 fn insp_english_documentation() {}
 
+/// @SRS{ROS-OPER-010}
+///
+/// Inspection is described in N7S-ROS-SVSR-001, chapter 7.2.
+#[cfg_attr(not(doc), test)]
+fn insp_mission_scenario_established() {}
+
+/// @SRS{ROS-OPER-020}
+///
+/// Inspection is described in N7S-ROS-SVSR-001, chapter 7.2.
+#[cfg_attr(not(doc), test)]
+fn insp_mission_scenario_exercises_uart() {}
+
+/// @SRS{ROS-OPER-030}
+///
+/// Inspection is described in N7S-ROS-SVSR-001, chapter 7.2.
+#[cfg_attr(not(doc), test)]
+fn insp_mission_scenario_exercises_spi() {}
+
+/// @SRS{ROS-OPER-040}
+///
+/// Inspection is described in N7S-ROS-SVSR-001, chapter 7.2.
+#[cfg_attr(not(doc), test)]
+fn insp_mission_scenario_exercises_execution_stats() {}
+
 /// @SRS{ROS-DES-030}
 ///
 /// Inspection is described in N7S-ROS-SVSR-001, chapter 7.2.
