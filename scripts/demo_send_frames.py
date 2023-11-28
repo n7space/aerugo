7fdcb4ce54c8239ae023e6233c86de4da724a938
Wojciech Olech <wojciech.olech@n7space.com>
2023-11-28 15:05:58 +0100

    Demo: Added delays to test script and changed baudrate

--- scripts/demo_send_frames.py
+++ scripts/demo_send_frames.py
@@ -1,6 +1,7 @@
 """Helper script for Aerugo SPI accelerometer demonstration application that sends testing frames"""
 import logging
 import sys
+from time import sleep
 
 from calldwell import init_default_logger
 from calldwell.ssh_client import SSHClient
@@ -13,7 +14,7 @@ from scripts.env import (
     BOARD_UART_PORT,
 )
 
-ECHO_EXAMPLE_BAUDRATE = 9600
+ECHO_EXAMPLE_BAUDRATE = 57600
 
 
 def main() -> None:
@@ -34,16 +35,22 @@ def main() -> None:
 
     logging.info("Sending opcode 0x00")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x00, 0x00, 0x01, 0x00]))
+    sleep(0.1)
     logging.info("Sending opcode 0x10")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x10, 0x00, 0x01, 0x00]))
+    sleep(0.1)
     logging.info("Sending opcode 0x20")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x20, 0x00, 0x01, 0x00]))
+    sleep(0.1)
     logging.info("Sending opcode 0x30")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x30, 0x00, 0x01, 0x08]))
+    sleep(0.1)
     logging.info("Sending opcode 0x40")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x40, 0x00, 0x01, 0xBE]))
+    sleep(0.1)
     logging.info("Sending opcode 0x50")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x50, 0x00, 0x01, 0xEF]))
+    sleep(0.1)
     logging.info("Sending opcode 0x60")
     uart.write_bytes(bytes([0x10, 0x00, 0xC0, 0x60, 0x00, 0x01, 0x00]))
     logging.info("All done!")
